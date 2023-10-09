f92fceff8ccd689ec736f80028e003221d80d27f
EduardoAnzardo <104447964+EduardoAnzardo@users.noreply.github.com>
2023-10-09 23:48:15 +0000

    dependencia

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tapr-2023-equipe1-curso-java
+
+## Autenticação no AZURE
+[DOC](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt)
+
+```
+az login -u eduardo.anzardo@univille.br
+az ad signed-in-user show
+```
---
+++ microservcurso/src/main/java/br/edu/univille/microservcurso/MicroservcursoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.microservcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservcursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservcursoApplication.class, args);
+	}
+
+}
