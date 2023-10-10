52b3111c3b1a63c5bb6330de6198eb85acfe0f68
EduardoAnzardo <104447964+EduardoAnzardo@users.noreply.github.com>
2023-10-10 01:02:41 +0000

    desenvolvimento entity

--- microservcurso/src/main/java/br/edu/univille/microservcurso/entity/Docente.java
+++
@@ -1,5 +0,0 @@
-package br.edu.univille.microservcurso.entity;
-
-public class Docente {
-    
-}
---
+++ microservcurso/src/main/java/br/edu/univille/microservcurso/entity/Professor.java
@@ -0,0 +1,48 @@
+package br.edu.univille.microservcurso.entity;
+
+import org.springframework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+
+@Container(containerName = "Professor")
+public class Professor {
+    @Id
+    @GeneratedValue
+    public String id;
+    @PartitionKey
+    public String nome;
+    public String curso;
+    public String turma;
+
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+}
